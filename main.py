068c8c433e140a09e291a791fcf6d9fd11b8f7e2
Fortuz <fortuz19@gmail.com>
2026-04-07 15:49:09 +0200

    second main file deleted

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from db-magic!")
-
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
 # Main code goes here
+
+def main():
+    print("Hello, DB Magic!") 
+
+if __name__ == "__main__":
+    main()  
